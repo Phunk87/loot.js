88622701e5be05ac7d9bc49666b24d39286beb7c
0x84A <0x84A@2100.io>
2021-09-07 14:01:05 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -11,38 +11,43 @@ Class Loot {
     const loot = new ethers.Contract(lootAddress, lootABI, rpc);
     const moreLoot = new ethers.Contract(moreLoot, lootABI, rpc);
 
-    this.rpc = rpc;
     this.loot = loot;
     this.moreLoot = moreLoot;
   }
 
   async function getBag(lootId) {
-    const [chest, foot, hand, head, neck, ring, waist, weapon, owner] =
-      await Promise.all([
-        loot.getChest(i),
-        loot.getFoot(i),
-        loot.getHand(i),
-        loot.getHead(i),
-        loot.getNeck(i),
-        loot.getRing(i),
-        loot.getWaist(i),
-        loot.getWeapon(i),
-        loot.ownerOf(i)
-      ]);
+    if (lootId > 0) {
+      let loot = (lootId < 8001) ? this.loot : this.moreLoot;
 
-    let bag = {
-      chest: chest,
-      foot: foot,
-      hand: hand,
-      head: head,
-      neck: neck,
-      ring: ring,
-      waist: waist,
-      weapon: weapon,
-      owner: owner
+      const [chest, foot, hand, head, neck, ring, waist, weapon, owner] =
+        await Promise.all([
+          loot.getChest(i),
+          loot.getFoot(i),
+          loot.getHand(i),
+          loot.getHead(i),
+          loot.getNeck(i),
+          loot.getRing(i),
+          loot.getWaist(i),
+          loot.getWeapon(i),
+          loot.ownerOf(i)
+        ]);
+
+      let bag = {
+        chest: chest,
+        foot: foot,
+        hand: hand,
+        head: head,
+        neck: neck,
+        ring: ring,
+        waist: waist,
+        weapon: weapon,
+        owner: owner
+      }
+
+      return bag;
     }
 
-    return bag;
+    return {};
   }
 
 }
