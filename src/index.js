da6955b38ef96c8d1eaad1794a6eccb6291a5fe7
0x84A <0x84A@2100.io>
2021-09-07 13:25:28 +0800

    Add Loot class

---
+++ src/index.js
@@ -0,0 +1,50 @@
+
+const ethers = require("ethers");
+const { lootABI } = require("./abi.js");
+const lootAddress = "0xFF9C1b15B16263C61d017ee9F65C50e4AE0113D7";
+const moreLootAddress = "0x1dfe7ca09e99d10835bf73044a23b73fc20623df";
+
+Class Loot {
+
+  constructor(rpcProvider) {
+    const rpc = new ethers.providers.JsonRpcProvider(rpcProvider);
+    const loot = new ethers.Contract(lootAddress, lootABI, rpc);
+    const moreLoot = new ethers.Contract(moreLoot, lootABI, rpc);
+
+    this.rpc = rpc;
+    this.loot = loot;
+    this.moreLoot = moreLoot;
+  }
+
+  async function getBag(lootId) {
+    const [chest, foot, hand, head, neck, ring, waist, weapon, owner] =
+      await Promise.all([
+        loot.getChest(i),
+        loot.getFoot(i),
+        loot.getHand(i),
+        loot.getHead(i),
+        loot.getNeck(i),
+        loot.getRing(i),
+        loot.getWaist(i),
+        loot.getWeapon(i),
+        loot.ownerOf(i)
+      ]);
+
+    let bag = {
+      chest: chest,
+      foot: foot,
+      hand: hand,
+      head: head,
+      neck: neck,
+      ring: ring,
+      waist: waist,
+      weapon: weapon,
+      owner: owner
+    }
+
+    return bag;
+  }
+
+}
+
+module.exports = Loot;
