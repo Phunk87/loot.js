1dc60a81406e1e4b59563d560c2bffc908a7bbcc
0x84A <0x84A@2100.io>
2021-09-07 14:57:25 +0800

    Add methods

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # loot.js
+
+## Get started
+
+```javascript
+var Loot = require('loot.js');
+
+// init
+let loot = new Loot("http://localhost:8545");
+
+// get bag
+var bag = await loot.bag(1000);
+console.log(bag);
+
+// get wallet lootIds
+var lootIds = await loot.lootIdsInWallet("0x84AB05F09B5ad3a1de6941FBf29BdF77CC7E2100");
+console.log(lootIds);
+```
--- src/index.js
+++ src/index.js
@@ -4,32 +4,31 @@ const { lootABI } = require("./abi.js");
 const lootAddress = "0xFF9C1b15B16263C61d017ee9F65C50e4AE0113D7";
 const moreLootAddress = "0x1dfe7ca09e99d10835bf73044a23b73fc20623df";
 
-Class Loot {
+class Loot {
 
   constructor(rpcProvider) {
     const rpc = new ethers.providers.JsonRpcProvider(rpcProvider);
     const loot = new ethers.Contract(lootAddress, lootABI, rpc);
-    const moreLoot = new ethers.Contract(moreLoot, lootABI, rpc);
+    const moreLoot = new ethers.Contract(moreLootAddress, lootABI, rpc);
 
     this.loot = loot;
     this.moreLoot = moreLoot;
   }
 
-  async function getBag(lootId) {
+  async bag(lootId) {
     if (lootId > 0) {
       let loot = (lootId < 8001) ? this.loot : this.moreLoot;
 
-      const [chest, foot, hand, head, neck, ring, waist, weapon, owner] =
+      const [chest, foot, hand, head, neck, ring, waist, weapon] =
         await Promise.all([
-          loot.getChest(i),
-          loot.getFoot(i),
-          loot.getHand(i),
-          loot.getHead(i),
-          loot.getNeck(i),
-          loot.getRing(i),
-          loot.getWaist(i),
-          loot.getWeapon(i),
-          loot.ownerOf(i)
+          loot.getChest(lootId),
+          loot.getFoot(lootId),
+          loot.getHand(lootId),
+          loot.getHead(lootId),
+          loot.getNeck(lootId),
+          loot.getRing(lootId),
+          loot.getWaist(lootId),
+          loot.getWeapon(lootId),
         ]);
 
       let bag = {
@@ -41,7 +40,6 @@ Class Loot {
         ring: ring,
         waist: waist,
         weapon: weapon,
-        owner: owner
       }
 
       return bag;
@@ -50,6 +48,20 @@ Class Loot {
     return {};
   }
 
+  async lootIdsInWallet(address) {
+    const balance = await this.loot.balanceOf(address);
+    let lootIds = [];
+    for (var i = 0;i < balance; i++) {
+      let lootId = await this.loot.tokenOfOwnerByIndex(address, i);
+      lootIds.push(lootId.toString());
+    }
+
+    return lootIds;
+  }
+
+  async build(address) {
+
+  }
 }
 
 module.exports = Loot;
